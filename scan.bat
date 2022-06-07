1056ac6b51c0e9ea7a576e0b6d95aa4e328888db
nfxpnk <nfxpnk@gmail.com>
2022-06-07 18:14:19 +0300

    scan color id

--- scan.bat
+++ scan.bat
@@ -1,4 +1,13 @@
 @echo off
+
+if %1==grey (
+	set color=2
+) else (
+	set color=1
+)
+
+echo Scan color id: %color%
+
 set configPath=%~dp0config.bat
 call "%configPath%"
 
@@ -9,7 +18,6 @@ for /f "tokens=1-4 delims=/. " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
 for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
 for /f "usebackq delims=" %%x in ("%counterFilePath%") do set counter=%%x
-echo %%x
 echo %counter%
 set /A counter=counter+1
 echo %counter%
@@ -18,7 +26,7 @@ set BmpFilePath=%savePath%%counter%_%mydate%_%mytime%.bmp
 
 echo Scanned file path: %BmpFilePath%
 
-"C:/Windows/System32/cscript.exe" //X "%vbsFilePath%" "%BmpFilePath%" 2
+"C:/Windows/System32/cscript.exe" //X "%vbsFilePath%" "%BmpFilePath%" %color%
 
 @echo %counter% > "%counterFilePath%"
 
